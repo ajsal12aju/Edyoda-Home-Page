a8bc283a00bcf305175b785f01a00e8312184613
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-10-07 01:08:49 +0530

    navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+       <header>
+    <div class="header-div">
+    <h1 class="head-h1">
+       EDYODA
+      <strong><i>Stories</i></strong>
+ 
+</h1>
+      <span>
+  <select class="head-selection">
+    <option value="option1">Explore Categories</option>
+    <option value="option2">Python</option>
+    <option value="option3">java</option>
+    <option value="option3">javascript</option>
+  </select>
+</span>
+
+      
+    </div>
+  
+
+    </header>
+    </div>
+  )
+}
+
+export default Navbar
