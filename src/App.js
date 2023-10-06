39cd72461b63350330279ba519c586cdcb02c582
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-10-07 01:12:08 +0530

    category

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Category from './Components/Category';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+
+<Category/>
     </div>
   );
 }
---
+++ src/Components/Category.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Category() {
+  return (
+    <div>
+         <div class="article-div">
+   <h1 class="article-head">Trending Posts</h1>
+      
+      <i class="fa-solid fa-filter filter"></i><span class="filter-text">Filtter by Category</span>
+        <article class="button-class">
+          <a class="art-btn">All</a>
+           <a class="art-btn">Artificiol Inteligence</a>
+          <a class="art-btn">Cloud Computing</a>
+          <a class="art-btn">DevOps</a>
+          
+          <a class="art-btn">Programing Languages</a>
+          <a class="art-btn">Mobile Application Development</a>
+          <a class="art-btn">Technology and Tools</a>
+          <a class="art-btn">Get a Job Tech Companies</a>
+          <a class="art-btn">Others</a>
+          
+         
+  </article>
+      
+      </div>
+    </div>
+  )
+}
+
+export default Category
